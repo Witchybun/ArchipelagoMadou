5361089c8f80ad7cbff8021f4440d25a91ad6bef
Witchybun <elnendil@gmail.com>
2023-11-18 17:17:22 -0600

    Include ItemClassification in Data

--- worlds/lunacid/data/spell_data.py
+++ worlds/lunacid/data/spell_data.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import List
+from BaseClasses import ItemClassification
 
 from .game_item import GameItem
 from ..strings.spells import Spell
@@ -9,48 +11,50 @@ from ..strings.regions_entrances import Region
 class SpellItem(GameItem):
     name: str
     region: str
+    classification: ItemClassification
 
     def __repr__(self):
-        return f"{self.name} (Located At: {self.region} |"
+        return f"{self.name} (Located At: {self.region} |" \
+               f"(Classification: {self.classification})"
 
 
-all_spells = []
+all_spells: List[SpellItem] = []
 
 
-def create_spell(name: str, region: str):
-    spell = SpellItem(name, region)
+def create_spell(name: str, region: str, classification: ItemClassification):
+    spell = SpellItem(name, region, classification)
     all_spells.append(spell)
     return spell
 
 
-barrier = create_spell(Spell.barrier, Region.fetid_mire)
-bestial_communion = create_spell(Spell.bestial_communion, Region.daedalus)
-blood_drain = create_spell(Spell.blood_drain, Region.yosei_forest)
-blood_strike = create_spell(Spell.blood_strike, Region.hollow_basin)
-blue_flame_arc = create_spell(Spell.blue_flame_arc, Region.terminus_prison)
-coffin = create_spell(Spell.coffin, Region.accursed_tomb)
-corpse_transformation = create_spell(Spell.corpse_transformation, Region.forbidden_archives)
-earth_strike = create_spell(Spell.earth_strike, Region.yosei_forest)
-earth_thorn = create_spell(Spell.earth_thorn, Region.sealed_ballroom)
-fire_worm = create_spell(Spell.fire_worm, Region.daedalus)
-flame_flare = create_spell(Spell.flame_flare, Region.hollow_basin)
-flame_spear = create_spell(Spell.flame_spear, Region.hollow_basin)
-ghost_light = create_spell(Spell.ghost_light, Region.hollow_basin)
-holy_warmth = create_spell(Spell.holy_warmth, Region.yosei_forest)
-icarian_flight = create_spell(Spell.icarian_flight, Region.terminus_prison)
-ice_spear = create_spell(Spell.ice_spear, Region.fetid_mire)
-ice_tear = create_spell(Spell.ice_tear, Region.laetus_chasm)
-ignis_calor = create_spell(Spell.ignis_calor, Region.boiling_grotto)
-lava_chasm = create_spell(Spell.lava_chasm, Region.labyrinth_of_ash)
-light_reveal = create_spell(Spell.light_reveal, Region.yosei_forest)
-lightning = create_spell(Spell.lightning, Region.accursed_tomb)
-lithomancy = create_spell(Spell.lithomancy, Region.hollow_basin)
-moon_beam = create_spell(Spell.moon_beam, Region.daedalus)
-poison_mist = create_spell(Spell.poison_mist, Region.forest_canopy)
-rock_bridge = create_spell(Spell.rock_bridge, Region.boiling_grotto)
-slime_orb = create_spell(Spell.slime_orb, Region.fetid_mire)
-spirit_warp = create_spell(Spell.spirit_warp, Region.labyrinth_of_ash)
-summon_fairy = create_spell(Spell.summon_fairy, Region.sanguine_sea)
-summon_ice_sword = create_spell(Spell.summon_ice_sword, Region.castle_le_fanu_red)
-wind_dash = create_spell(Spell.wind_dash, Region.fetid_mire)
-wind_slicer = create_spell(Spell.wind_slicer, Region.forest_canopy)
+barrier = create_spell(Spell.barrier, Region.fetid_mire, ItemClassification.useful)
+bestial_communion = create_spell(Spell.bestial_communion, Region.daedalus, ItemClassification.useful)
+blood_drain = create_spell(Spell.blood_drain, Region.yosei_forest, ItemClassification.progression)
+blood_strike = create_spell(Spell.blood_strike, Region.hollow_basin, ItemClassification.progression)
+blue_flame_arc = create_spell(Spell.blue_flame_arc, Region.terminus_prison, ItemClassification.useful)
+coffin = create_spell(Spell.coffin, Region.accursed_tomb, ItemClassification.progression)
+corpse_transformation = create_spell(Spell.corpse_transformation, Region.forbidden_archives, ItemClassification.useful)
+earth_strike = create_spell(Spell.earth_strike, Region.yosei_forest, ItemClassification.useful)
+earth_thorn = create_spell(Spell.earth_thorn, Region.sealed_ballroom, ItemClassification.useful)
+fire_worm = create_spell(Spell.fire_worm, Region.daedalus, ItemClassification.useful)
+flame_flare = create_spell(Spell.flame_flare, Region.hollow_basin, ItemClassification.useful)
+flame_spear = create_spell(Spell.flame_spear, Region.hollow_basin, ItemClassification.useful)
+ghost_light = create_spell(Spell.ghost_light, Region.hollow_basin, ItemClassification.progression)
+holy_warmth = create_spell(Spell.holy_warmth, Region.yosei_forest, ItemClassification.useful)
+icarian_flight = create_spell(Spell.icarian_flight, Region.terminus_prison, ItemClassification.progression)
+ice_spear = create_spell(Spell.ice_spear, Region.fetid_mire, ItemClassification.useful)
+ice_tear = create_spell(Spell.ice_tear, Region.laetus_chasm, ItemClassification.useful)
+ignis_calor = create_spell(Spell.ignis_calor, Region.boiling_grotto, ItemClassification.progression)
+lava_chasm = create_spell(Spell.lava_chasm, Region.labyrinth_of_ash, ItemClassification.useful)
+light_reveal = create_spell(Spell.light_reveal, Region.yosei_forest, ItemClassification.useful)
+lightning = create_spell(Spell.lightning, Region.accursed_tomb, ItemClassification.useful)
+lithomancy = create_spell(Spell.lithomancy, Region.hollow_basin, ItemClassification.useful)
+moon_beam = create_spell(Spell.moon_beam, Region.daedalus, ItemClassification.useful)
+poison_mist = create_spell(Spell.poison_mist, Region.forest_canopy, ItemClassification.useful)
+rock_bridge = create_spell(Spell.rock_bridge, Region.boiling_grotto, ItemClassification.progression)
+slime_orb = create_spell(Spell.slime_orb, Region.fetid_mire, ItemClassification.useful)
+spirit_warp = create_spell(Spell.spirit_warp, Region.labyrinth_of_ash, ItemClassification.useful)
+summon_fairy = create_spell(Spell.summon_fairy, Region.sanguine_sea, ItemClassification.useful)
+summon_ice_sword = create_spell(Spell.summon_ice_sword, Region.castle_le_fanu_red, ItemClassification.useful)
+wind_dash = create_spell(Spell.wind_dash, Region.fetid_mire, ItemClassification.useful)
+wind_slicer = create_spell(Spell.wind_slicer, Region.forest_canopy, ItemClassification.useful)
--- worlds/lunacid/data/weapon_data.py
+++ worlds/lunacid/data/weapon_data.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from BaseClasses import ItemClassification
+from typing import List
 
 from .game_item import GameItem
 from ..strings.weapons import Weapon
@@ -9,48 +11,51 @@ from ..strings.regions_entrances import Region
 class WeaponItem(GameItem):
     name: str
     region: str
+    classification: ItemClassification
+
 
     def __repr__(self):
-        return f"{self.name} (Located At: {self.region} |"
+        return f"{self.name} (Located At: {self.region}) |" \
+               f"(Classification: {self.classification})"
 
 
-all_weapons = []
+all_weapons: List[WeaponItem] = []
 
 
-def create_weapon(name: str, region: str):
-    weapon = WeaponItem(name, region)
+def create_weapon(name: str, region: str, classification: ItemClassification):
+    weapon = WeaponItem(name, region, classification)
     all_weapons.append(weapon)
     return weapon
 
 
-axe_of_harming = create_weapon(Weapon.axe_of_harming, Region.forbidden_archives)
-battle_axe = create_weapon(Weapon.battle_axe, Region.fetid_mire)
-blade_of_jusztina = create_weapon(Weapon.blade_of_jusztina, Region.accursed_tomb)
-blade_of_ophelia = create_weapon(Weapon.blade_of_ophelia, Region.castle_le_fanu_blue)
-blessed_wind = create_weapon(Weapon.blessed_wind, Region.laetus_chasm)
-broken_hilt = create_weapon(Weapon.broken_hilt, Region.fetid_mire)
-broken_lance = create_weapon(Weapon.broken_lance, Region.terminus_prison)
-corrupted_dagger = create_weapon(Weapon.corrupted_dagger, Region.sanguine_sea)
-dark_rapier = create_weapon(Weapon.dark_rapier, Region.sanguine_sea)
-elfen_bow = create_weapon(Weapon.elfen_bow, Region.yosei_forest)
-elfen_sword = create_weapon(Weapon.elfen_sword, Region.yosei_forest)
-fishing_spear = create_weapon(Weapon.fishing_spear, Region.terminus_prison)
-flail = create_weapon(Weapon.flail, Region.sealed_ballroom)
-halberd = create_weapon(Weapon.halberd, Region.accursed_tomb)
-iron_claw = create_weapon(Weapon.iron_claw, Region.boiling_grotto)
-moonlight = create_weapon(Weapon.moonlight, Region.tower_abyss)
-obsidian_seal = create_weapon(Weapon.obsidian_seal, Region.forlorn_arena)
-replica_sword = create_weapon(Weapon.replica_sword, Region.hollow_basin)
-ritual_dagger = create_weapon(Weapon.ritual_dagger, Region.hollow_basin)
-serpent_fang = create_weapon(Weapon.serpent_fang, Region.labyrinth_of_ash)
-shadow_blade = create_weapon(Weapon.shadow_blade, Region.forlorn_arena)
-steel_spear = create_weapon(Weapon.steel_spear, Region.hollow_basin)
-stone_club = create_weapon(Weapon.stone_club, Region.hollow_basin)
-torch = create_weapon(Weapon.torch, Region.hollow_basin)
-twisted_staff = create_weapon(Weapon.torch, Region.accursed_tomb)
-vampire_hunter_sword = create_weapon(Weapon.vampire_hunter_sword, Region.accursed_tomb)
-wand_of_power = create_weapon(Weapon.wand_of_power, Region.castle_le_fanu_red)
-wolfram_greatsword = create_weapon(Weapon.wolfram_greatsword, Region.forbidden_archives)
-wooden_shield = create_weapon(Weapon.wooden_shield, Region.hollow_basin)
+axe_of_harming = create_weapon(Weapon.axe_of_harming, Region.forbidden_archives, ItemClassification.useful)
+battle_axe = create_weapon(Weapon.battle_axe, Region.fetid_mire, ItemClassification.useful)
+blade_of_jusztina = create_weapon(Weapon.blade_of_jusztina, Region.accursed_tomb, ItemClassification.useful)
+blade_of_ophelia = create_weapon(Weapon.blade_of_ophelia, Region.castle_le_fanu_blue, ItemClassification.useful)
+blessed_wind = create_weapon(Weapon.blessed_wind, Region.laetus_chasm, ItemClassification.useful)
+broken_hilt = create_weapon(Weapon.broken_hilt, Region.fetid_mire, ItemClassification.useful)
+broken_lance = create_weapon(Weapon.broken_lance, Region.terminus_prison, ItemClassification.useful)
+corrupted_dagger = create_weapon(Weapon.corrupted_dagger, Region.sanguine_sea, ItemClassification.useful)
+dark_rapier = create_weapon(Weapon.dark_rapier, Region.sanguine_sea, ItemClassification.useful)
+elfen_bow = create_weapon(Weapon.elfen_bow, Region.yosei_forest, ItemClassification.useful)
+elfen_sword = create_weapon(Weapon.elfen_sword, Region.yosei_forest, ItemClassification.useful)
+fishing_spear = create_weapon(Weapon.fishing_spear, Region.terminus_prison, ItemClassification.useful)
+flail = create_weapon(Weapon.flail, Region.sealed_ballroom, ItemClassification.useful)
+halberd = create_weapon(Weapon.halberd, Region.accursed_tomb, ItemClassification.useful)
+iron_claw = create_weapon(Weapon.iron_claw, Region.boiling_grotto, ItemClassification.useful)
+moonlight = create_weapon(Weapon.moonlight, Region.tower_abyss, ItemClassification.useful)
+obsidian_seal = create_weapon(Weapon.obsidian_seal, Region.forlorn_arena, ItemClassification.useful)
+replica_sword = create_weapon(Weapon.replica_sword, Region.hollow_basin, ItemClassification.useful)
+ritual_dagger = create_weapon(Weapon.ritual_dagger, Region.hollow_basin, ItemClassification.useful)
+serpent_fang = create_weapon(Weapon.serpent_fang, Region.labyrinth_of_ash, ItemClassification.useful)
+shadow_blade = create_weapon(Weapon.shadow_blade, Region.forlorn_arena, ItemClassification.useful)
+steel_spear = create_weapon(Weapon.steel_spear, Region.hollow_basin, ItemClassification.useful)
+stone_club = create_weapon(Weapon.stone_club, Region.hollow_basin, ItemClassification.useful)
+torch = create_weapon(Weapon.torch, Region.hollow_basin, ItemClassification.progression)
+twisted_staff = create_weapon(Weapon.torch, Region.accursed_tomb, ItemClassification.progression)
+vampire_hunter_sword = create_weapon(Weapon.vampire_hunter_sword, Region.accursed_tomb, ItemClassification.useful)
+wand_of_power = create_weapon(Weapon.wand_of_power, Region.castle_le_fanu_red, ItemClassification.useful)
+wolfram_greatsword = create_weapon(Weapon.wolfram_greatsword, Region.forbidden_archives, ItemClassification.useful)
+wooden_shield = create_weapon(Weapon.wooden_shield, Region.hollow_basin, ItemClassification.useful)
 
 
