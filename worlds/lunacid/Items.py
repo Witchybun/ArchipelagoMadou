9e570241be10123184d301f77f77686b16c1a400
Witchybun <elnendil@gmail.com>
2023-11-24 06:24:15 -0600

    Fix item generation

--- worlds/lunacid/Items.py
+++ worlds/lunacid/Items.py
@@ -1,9 +1,7 @@
 from BaseClasses import ItemClassification
 from typing import TypedDict, Dict, List
+from .strings.items import UniqueItem
 
-from .data.weapon_data import all_weapons
-from .data.spell_data import all_spells
-from .data.item_data import all_items
 from .Locations import item_total_table
 
 
@@ -16,18 +14,13 @@ class ItemDict(TypedDict):
 base_id = 771111110
 
 item_table: List[ItemDict] = []
-for weapon in all_weapons:
-    item_table.append({'name': weapon.name, 'count': item_total_table[weapon.name], 'classification': weapon.classification})
-for spell in all_spells:
-    item_table.append({'name': spell.name, 'count': item_total_table[spell.name], 'classification': spell.classification})
-for item in all_items:
-    item_table.append({'name': item.name, 'count': item_total_table[item.name], 'classification': item.classification})
+for items in item_total_table:
+    item_table.append({'name': items.name, 'count': item_total_table[items], 'classification': items.classification})
 
 group_table: Dict[str, List[str]] = {
-    "vampiric symbol": ["Vampiric Symbol (W)",
-                        "Vampiric Symbol (A)",
-                        "Vampiric Symbol (E)"],
+    "vampiric symbol": [UniqueItem.vampiric_symbol_a, UniqueItem.vampiric_symbol_e, UniqueItem.vampiric_symbol_w],
 
-    "vhs": ["VHS Tape",
-            "White VHS Tape"],
+    "vhs": [UniqueItem.vhs_tape, UniqueItem.white_tape],
+
+    "talisman": [UniqueItem.earth_talisman, UniqueItem.water_talisman]
 }
