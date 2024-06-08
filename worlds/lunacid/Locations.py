01ff787f54b6e9873482d7425adce6d1c07791d5
Witchybun <embenham05@gmail.com>
2024-06-08 02:49:48 -0500

    Return to 1 in IDs

--- worlds/lunacid/Locations.py
+++ worlds/lunacid/Locations.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from .data.location_data import base_locations, shop_locations, mob_drop_locations
 
-LOCATION_CODE_START = 771111110
-
 
 @dataclass(frozen=True)
 class LocationDict:
@@ -16,12 +14,12 @@ def initialize_locations():
     loc_2 = []
     loc_3 = []
     for location in base_locations:
-        loc_1.append(LocationDict(LOCATION_CODE_START + location.id, location.name, location.region))
+        loc_1.append(LocationDict(location.id, location.name, location.region))
     base_locations_cleaned = loc_1
     for location in shop_locations:
-        loc_2.append(LocationDict(LOCATION_CODE_START + location.id, location.name, location.region))
+        loc_2.append(LocationDict(location.id, location.name, location.region))
     for location in mob_drop_locations:
-        loc_3.append(LocationDict(LOCATION_CODE_START + location.id, location.name, location.region))
+        loc_3.append(LocationDict(location.id, location.name, location.region))
     shop_locations_cleaned = loc_2
     mob_drop_locations_cleaned = loc_3
     locations = loc_1 + loc_2 + loc_3
